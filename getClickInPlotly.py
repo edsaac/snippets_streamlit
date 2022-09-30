584fe35a1bc354b97588ba0b65662eefd9253c15
edsaa <esaavedrac@u.northwestern.edu>
2022-09-29 22:32:43 -0500

    missing zoom events :(

--- getClickInPlotly.py
+++ getClickInPlotly.py
@@ -4,6 +4,19 @@ from streamlit_plotly_events import plotly_events
 import plotly.graph_objects as go
 import numpy as np
 
+def printSelectedPoints(container, selected_points):
+    with container:
+        if selected_points:
+
+            ## Click event on a single figure
+            point = selected_points[0]
+
+            ## Where did we click?
+            st.header(":mouse: Where click?")
+            cols = st.columns(2)
+            with cols[0]: st.metric("X",point['x'])
+            with cols[1]: st.metric("Y",point['y'])
+
 corrMatrix = np.array(
        [[2, 6, 3, 3, 8, 0, 1, 7, 1],
        [3, 1, 5, 2, 9, 5, 9, 6, 6],
@@ -21,10 +34,17 @@ fig = go.Figure()
 heat = fig.add_heatmap(z=corrMatrix)
 selected_points = plotly_events(fig, click_event=True)
 
-## Click event on a single figure
-point = selected_points[0]
+pc1 = st.container()
+printSelectedPoints(pc1, selected_points)
 
-## Where did we click?
-st.header(":mouse: Where click?")
-st.metric("X",point['x'])
-st.metric("Y",point['y'])
+"-----------------------------------"
+import plotly.express as px
+from skimage import io
+
+img = io.imread("http://placekitten.com/g/400/500")
+fig = px.imshow(img)
+
+selected_points = plotly_events(fig, click_event=True, select_event=True)
+
+pc2 = st.container()
+printSelectedPoints(pc2, selected_points)
