5fd07777c6d4b75e60653df5cbb4c68da66d2797
edsaa <esaavedrac@u.northwestern.edu>
2022-10-04 15:37:38 -0500

    modify checkboxes not working

---
+++ modifyCheckboxes.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown(""" 
+    <style>
+    .stCheckbox [data-baseweb=checkbox] span{
+        background-image: url('https://cdn-icons-png.flaticon.com/512/891/891399.png');
+    }
+    </style>
+    """, unsafe_allow_html=True)
