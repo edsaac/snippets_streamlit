6017dc1b7cbac2b825e04c8ba824d36bec225085
edsaa <esaavedrac@u.northwestern.edu>
2022-10-12 21:49:43 -0500

    fixes

--- imgkitTest.py
+++ imgkitTest.py
@@ -1,7 +1,18 @@
 import streamlit as st
 import imgkit
 
-## Add some styling with CSS selectors 
+@st.cache
+def get_screeshot(url):
+    # Some options to pass to wkhtmltoimage
+    # (More info in the man)
+    options = {
+        '--crop-h': '500',
+        '--format': 'png',
+        '--enable-javascript': None,
+    }
+    return imgkit.from_url(url, False, options=options)
+
+# Add some styling with CSS selectors
 st.markdown("""
     <style>
     img {
@@ -21,7 +32,7 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-## Dict of URLS to screenshot
+# Dict of URLS to screenshot
 urls = {
     "Search engines": [
         'duckduckgo.com',
@@ -30,35 +41,24 @@ urls = {
     "Tools": [
         'isocpp.org',
         'wkhtmltopdf.org',
-        'www.python.org'],
-    "Linux distros": [
+        'www.python.org',
+        'placekitten.com',
+        'github.com'],
+    "Distros": [
         'www.debian.org',
         'linuxmint.com',
         'archlinux.org'
-    ],
-    "Repositories": [
-        'github.com',
-        'about.gitlab.com'
     ]}
-
-# Some options to pass to wkhtmltoimage
-# (More info in the man)
-options = {
-    '--crop-h':'500', 
-    '--format':'png',
-    '--enable-javascript': None,
-    }
 
 st.title("🖼️ Gallery of websites")
 
-cols = st.columns([1,2])
-with cols[0]: 
+cols = st.columns([1, 2])
+with cols[0]:
     category = st.radio("Categories", urls.keys())
 
 for url in urls[category]:
-    screenshot = imgkit.from_url(url, False, options=options)
-
-    with cols[1]: 
+    screenshot = get_screeshot(url)
+    with cols[1]:
         st.markdown(f"#### [{url}](https://{url})")
         st.image(screenshot, use_column_width=True)
         st.caption("Some additional description")
