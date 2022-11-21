adaa4cffabca10651ce3657b6b6e9d5e56a9d4f2
Ed Saa <esaavedrac@u.northwestern.edu>
2022-11-21 15:04:10 -0600

    echo path

--- systemInformation.py
+++ systemInformation.py
@@ -6,12 +6,16 @@ NCPUS = os.cpu_count()
 "# CPU"
 st.header(f"This machine has {NCPUS} CPU available")
 
+"# Disk"
+NDISK = subprocess.run(args=["df", "-H"], capture_output=True).stdout.decode('utf-8').splitlines()
+ldisk = "".join([l+'\n' for l in NDISK])
+st.code(f"{ldisk}")
+
+path = subprocess.run(args=["echo", "$PATH"], capture_output=True).stdout.decode('utf-8').splitlines()
+st.code("\n".join(path))
+
 "# Memory"
 NMEMORY = subprocess.run(args=["free", "-g"], capture_output=True).stdout.decode('utf-8').splitlines()
 lmemo = "".join([l+'\n' for l in NMEMORY])
 st.code(f"{lmemo}")
 
-"# Disk"
-NDISK = subprocess.run(args=["df", "-H"], capture_output=True).stdout.decode('utf-8').splitlines()
-ldisk = "".join([l+'\n' for l in NDISK])
-st.code(f"{ldisk}")
