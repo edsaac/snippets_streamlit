648e254cb51cdf71c5e0651a90b2b7837084c2a5
Ed Saa <esaavedrac@u.northwestern.edu>
2022-11-21 15:06:23 -0600

    check path

--- systemInformation.py
+++ systemInformation.py
@@ -11,8 +11,7 @@ NDISK = subprocess.run(args=["df", "-H"], capture_output=True).stdout.decode('ut
 ldisk = "".join([l+'\n' for l in NDISK])
 st.code(f"{ldisk}")
 
-path = subprocess.run(args=["echo", "$PATH"], capture_output=True).stdout.decode('utf-8').splitlines()
-st.code("\n".join(path))
+st.code(os.environ)
 
 "# Memory"
 NMEMORY = subprocess.run(args=["free", "-g"], capture_output=True).stdout.decode('utf-8').splitlines()
