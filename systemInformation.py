4749a38d23f65c74b3282d14518a42f2ddacd36d
Ed Saa <esaavedrac@u.northwestern.edu>
2022-11-21 15:14:48 -0600

    organize

--- systemInformation.py
+++ systemInformation.py
@@ -2,21 +2,20 @@ import streamlit as st
 import os 
 import subprocess
 
-NCPUS = os.cpu_count()
-"# CPU"
-st.header(f"This machine has {NCPUS} CPU available")
+"# Available resources"
+
+"## CPU"
+st.header(f"This machine has {os.cpu_count()} CPU available")
 lscpu = subprocess.run(args=["lscpu"], capture_output=True).stdout.decode('utf-8').splitlines()
 st.code("\n".join(lscpu))
 
-"# Disk"
-NDISK = subprocess.run(args=["df", "-H"], capture_output=True).stdout.decode('utf-8').splitlines()
-ldisk = "\n".join(NDISK)
+"## Disk"
+dfH = subprocess.run(args=["df", "-H"], capture_output=True).stdout.decode('utf-8').splitlines()
+ldisk = "\n".join(dfH)
 st.code(f"{ldisk}")
 
-st.code(os.environ)
-
-"# Memory"
-NMEMORY = subprocess.run(args=["free", "-g"], capture_output=True).stdout.decode('utf-8').splitlines()
-lmemo = "".join([l+'\n' for l in NMEMORY])
+"## Memory"
+freeg = subprocess.run(args=["free", "-g"], capture_output=True).stdout.decode('utf-8').splitlines()
+lmemo = "".join([l+'\n' for l in freeg])
 st.code(f"{lmemo}")
 
